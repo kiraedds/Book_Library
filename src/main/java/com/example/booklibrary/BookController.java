2a3e82203875568a39715f81eb54005302c55efe
kiraed <dsmaluch@gmail.com>
2023-10-03 16:32:44 +0200

    added some crud methods

--- src/main/java/com/example/booklibrary/BookController.java
+++ src/main/java/com/example/booklibrary/BookController.java
@@ -1,29 +1,62 @@
 package com.example.booklibrary;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/books")
 public class BookController {
 
     @Autowired
     BookRepository bookRepository;
 
-    @GetMapping("/books")
+    @GetMapping
     public List<Book> getAll(){
         return bookRepository.getAll();
     }
 
 
-    @GetMapping("/books/{id}")
+    @GetMapping("/{id}")
     public Book getById(@PathVariable("id") int id){
         return bookRepository.getById(id);
     }
 
+    @PostMapping
+    public int addBook (@RequestBody List<Book> books){
+        return bookRepository.save(books);
+    }
+    @PutMapping("/{id}")
+    public int update(@PathVariable("id") int id, @RequestBody Book updatedBook){
+        Book book = bookRepository.getById(id);
+        if(book != null){
+            book.setName(updatedBook.getName());
+            book.setRating(updatedBook.getRating());
+            bookRepository.update(book);
+            return 1;
+        }else{
+            return -1;
+            //zwroc odpowiedni kod bledu dla rest api 404?
+        }
+    }
 
+    @PatchMapping("/{id}")
+    public int partiallyUpdate(@PathVariable("id") int id, @RequestBody Book updatedBook){
+        Book book = bookRepository.getById(id);
 
+        if(book != null){
+            if(updatedBook.getName() != null) book.setName(updatedBook.getName());
+            if(updatedBook.getRating() > 0) book.setRating(updatedBook.getRating());
+
+            bookRepository.update(book);
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+    @DeleteMapping("/{id}")
+    public int delete(@PathVariable("id") int id){
+        return bookRepository.delete(id);
+    }
 }
--- src/main/java/com/example/booklibrary/BookRepository.java
+++ src/main/java/com/example/booklibrary/BookRepository.java
@@ -15,10 +15,26 @@ public class BookRepository {
     JdbcTemplate jdbcTemplate;
 
     public List<Book> getAll(){
-        return jdbcTemplate.query("SELECT id,book,rating FROM book", BeanPropertyRowMapper.newInstance(Book.class));
+        return jdbcTemplate.query("SELECT id,name,rating FROM book", BeanPropertyRowMapper.newInstance(Book.class));
     }
 
     public Book getById(int id){
         return jdbcTemplate.queryForObject("SELECT id,name,rating FROM book where id=?",BeanPropertyRowMapper.newInstance(Book.class),id);
+    }
+
+    public int save(List<Book> books) {
+        books.forEach(book -> jdbcTemplate.update("INSERT into book (name,rating) values(?,?)",
+                    book.getName(), book.getRating()
+                    ));
+
+        return 1;
+    }
+    public int update(Book book){
+        return jdbcTemplate.update("update book SET name=?, rating=? WHERE id=?",
+                book.getName(), book.getRating(), book.getId());
+    }
+
+    public int delete(int id) {
+        return jdbcTemplate.update("delete from book where id=?",id);
     }
 }
